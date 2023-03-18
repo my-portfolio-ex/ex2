c29008c9a07426bf2ee5892f1c8019506725ab81
alpha0212 <qkrcjfgml321@naver.com>
2023-03-18 17:58:42 +0900

    feat(pages): Animation after selecting one of the two

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,5 +1,34 @@
-import React from 'react';
+import React, { SyntheticEvent, useEffect, useState } from 'react';
 
 import * as S from './styled';
 
-export const MainPage: React.FC = () => <S.text>안녕하세요</S.text>;
+export const MainPage: React.FC = () => {
+  const [introduce, setIntroduce] = useState(false);
+  const [project, setProject] = useState(false);
+  const [anyClick, setAnyClick] = useState(false);
+  const SelectOnClick = (event: SyntheticEvent) => {
+    setAnyClick(true);
+    if (event.currentTarget.innerHTML.includes('프로젝트')) {
+      setProject(true);
+      setIntroduce(false);
+    } else {
+      setProject(false);
+      setIntroduce(true);
+    }
+  };
+  console.log(introduce, project);
+  return (
+    <S.MainWrapper anyClick={anyClick}>
+      <S.LeftSection introduce={introduce} project={project} onClick={SelectOnClick}>
+        <S.LeftTitle introduce={introduce} project={project} clicked={anyClick}>
+          소개
+        </S.LeftTitle>
+      </S.LeftSection>
+      <S.RightSection introduce={introduce} project={project} onClick={SelectOnClick}>
+        <S.RightTitle introduce={introduce} project={project} clicked={anyClick}>
+          프로젝트
+        </S.RightTitle>
+      </S.RightSection>
+    </S.MainWrapper>
+  );
+};
