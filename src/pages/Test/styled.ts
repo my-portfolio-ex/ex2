82b43ee00a0b7e1007559d5cefe9312ff8a1c96c
alpha0212 <qkrcjfgml321@naver.com>
2023-03-18 23:40:23 +0900

    test

--- src/pages/Test/styled.ts
+++ src/pages/Test/styled.ts
@@ -1,34 +1,7 @@
 import styled from '@emotion/styled';
-import Slider from 'react-slick';
-export const TestCode = styled(Slider)`
-  .slider {
-    margin: 20px auto;
-    text-align: center;
-  }
-  .slider div {
-    margin-right: 5px;
-  }
 
-  .slick-slide {
-    width: 300px;
-    height: 100px;
-    opacity: 0.6;
-    width: 20px;
-    background: red;
-    margin-left: 20px;
-  }
-
-  .slick-center {
-    width: 600px;
-    height: 100px;
-    display: block;
-    max-width: 10% !important;
-    max-height: 20% !important;
-    opacity: 1;
-  }
-
-  .slick-current {
-    width: 600px;
-    height: 100px;
-  }
+export const Card = styled.div`
+  width: 200px;
+  height: 100px;
+  background: red;
 `;
