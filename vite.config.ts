306fc0585dd321ed5fa7c97bed328e66595117d7
alpha0212 <qkrcjfgml321@naver.com>
2023-03-18 23:29:27 +0900

    chore: vite increase performance

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import viteCompression from 'vite-plugin-compression';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), viteCompression()],
   resolve: {
     alias: [
       { find: '@components', replacement: '/src/components' },
