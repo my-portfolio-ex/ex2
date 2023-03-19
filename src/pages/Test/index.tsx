e20c8f632bbe0b01ef49dc7039b646596a69cd5f
alpha0212 <qkrcjfgml321@naver.com>
2023-03-19 21:02:24 +0900

    test

--- src/pages/Test/index.tsx
+++ src/pages/Test/index.tsx
@@ -1,7 +1,25 @@
 import React from 'react';
 
 import * as S from './styled';
-
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/swiper.scss';
+import 'swiper/components/navigation/navigation.scss';
+import 'swiper/components/pagination/pagination.scss';
 export const Test: React.FC = () => {
-  return <></>;
+  return (
+    <div>
+      <Swiper
+        className="banner"
+        spaceBetween={50}
+        slidesPerView={1}
+        navigation
+        pagination={{ clickable: true }}
+      >
+        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+      </Swiper>
+    </div>
+  );
 };
--- src/pages/Test/styled.ts
+++ src/pages/Test/styled.ts
@@ -1,7 +1,1 @@
 import styled from '@emotion/styled';
-
-export const Card = styled.div`
-  width: 200px;
-  height: 100px;
-  background: red;
-`;
