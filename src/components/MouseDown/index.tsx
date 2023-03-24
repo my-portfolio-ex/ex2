312870298f251ae29711fd97d9bb386a0fac64f0
alpha0212 <qkrcjfgml321@naver.com>
2023-03-24 22:18:52 +0900

    feat(component): Advise scroll down

---
+++ src/components/MouseDown/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import * as S from "./styled";
+
+export const MouseDown: React.FC = () => {
+  return (
+    <S.ScrollDown>
+      <S.Chevrons>
+        <S.ChevronDown></S.ChevronDown>
+        <S.ChevronDown></S.ChevronDown>
+      </S.Chevrons>
+    </S.ScrollDown>
+  );
+};
