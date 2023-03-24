a845774fdaf66f6378d8930a39949bf0b9d814f0
alpha0212 <qkrcjfgml321@naver.com>
2023-03-24 22:06:09 +0900

    feat(App.tsx): Add project path

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import { MainPage, Test } from '@/pages';
+import { MainPage, Test, ProjectPage } from '@/pages';
 
 export const App: React.FC = () => {
   return (
     <Routes>
       <Route index element={<MainPage />} />
       <Route path="test" element={<Test />} />
+      <Route path="project" element={<ProjectPage />} />
     </Routes>
   );
 };
