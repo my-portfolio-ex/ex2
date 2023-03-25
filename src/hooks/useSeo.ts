ec54d4678f1f55573c026510770b6fd455c97977
alpha0212 <qkrcjfgml321@naver.com>
2023-03-25 12:39:06 +0900

    build: strict mode false

--- src/hooks/useSeo.ts
+++ src/hooks/useSeo.ts
@@ -3,7 +3,7 @@ import { useEffect, useState } from 'react';
 export const useSeo = (TitleData: string | null) => {
   const [title, setTitle] = useState(TitleData);
   const updateTitle = () => {
-    const htmlTitle = document.querySelector('title');
+    const htmlTitle: HTMLTitleElement | null = document.querySelector('title');
     htmlTitle.text = title + `${' '}| 포트폴리오`;
   };
   useEffect(updateTitle, [title]);
