2486d0f51deb30a6990486e499461e4bafd5a351
alpha0212 <qkrcjfgml321@naver.com>
2023-03-26 19:43:33 +0900

    fix(hook): Modify useSeo text

--- src/hooks/useSeo.ts
+++ src/hooks/useSeo.ts
@@ -4,7 +4,7 @@ export const useSeo = (TitleData: string | null) => {
   const [title, setTitle] = useState(TitleData);
   const updateTitle = () => {
     const htmlTitle: HTMLTitleElement | null = document.querySelector('title');
-    htmlTitle.text = title + `${' '}| 포트폴리오`;
+    htmlTitle.text = title + `${' '}`;
   };
   useEffect(updateTitle, [title]);
 };
