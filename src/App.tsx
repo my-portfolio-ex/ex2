98ce1be46c9d125322394967c728d00f07290f16
alpha0212 <qkrcjfgml321@naver.com>
2023-03-26 19:41:02 +0900

    feat(App.tsx): Add introduce my self page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import { MainPage, Test, ProjectPage } from '@/pages';
+import { MainPage, Test, ProjectPage, IntroducePage } from '@/pages';
 
 export const App: React.FC = () => {
   return (
@@ -8,6 +8,7 @@ export const App: React.FC = () => {
       <Route index element={<MainPage />} />
       <Route path="test" element={<Test />} />
       <Route path="project" element={<ProjectPage />} />
+      <Route path="introduce" element={<IntroducePage />} />
     </Routes>
   );
 };
