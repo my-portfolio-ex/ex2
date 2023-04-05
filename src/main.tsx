707d9cd045e7582288d7e43565599a8cd6db5bda
alpha0212 <qkrcjfgml321@naver.com>
2023-04-05 23:56:41 +0900

    fix: css apply to add global styles

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { App } from './App';
+import { Global } from '@emotion/react';
+import { globalStyle } from './styles/global';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
+      <Global styles={globalStyle} />
       <App />
     </BrowserRouter>
   </React.StrictMode>
