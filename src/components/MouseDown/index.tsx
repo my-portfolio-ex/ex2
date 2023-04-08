c458c8e129198d415e3ac716b8951a0ac5bdbd7e
alpha0212 <qkrcjfgml321@naver.com>
2023-04-09 01:23:44 +0900

    feat(component): add description in Mousedown component

--- src/components/MouseDown/index.tsx
+++ src/components/MouseDown/index.tsx
@@ -3,11 +3,14 @@ import * as S from './styled';
 
 export const MouseDown: React.FC = () => {
   return (
-    <S.ScrollDown>
-      <S.Chevrons>
-        <S.ChevronDown></S.ChevronDown>
-        <S.ChevronDown></S.ChevronDown>
-      </S.Chevrons>
-    </S.ScrollDown>
+    <>
+      <S.ScrollDown>
+        <S.Chevrons>
+          <S.ChevronDown></S.ChevronDown>
+          <S.ChevronDown></S.ChevronDown>
+        </S.Chevrons>
+      </S.ScrollDown>
+      <h4 style={{ fontSize: '10px', marginTop: '50px' }}>천천히 스크롫 해주세요</h4>
+    </>
   );
 };
