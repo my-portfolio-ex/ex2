f8eb65ba6b6aa7009e06ef949bb0a25ece4cfeb3
alpha0212 <qkrcjfgml321@naver.com>
2023-04-10 05:24:07 +0900

    refactor: modify hook

--- src/hooks/useSeo.ts
+++ src/hooks/useSeo.ts
@@ -1,10 +1,12 @@
 import { useEffect, useState } from 'react';
 
 export const useSeo = (TitleData: string | null) => {
-  const [title, setTitle] = useState(TitleData);
+  const [title, setTitle] = useState(TitleData || '');
   const updateTitle = () => {
     const htmlTitle: HTMLTitleElement | null = document.querySelector('title');
-    htmlTitle.text = title + `${' '}`;
+    if (htmlTitle) {
+      htmlTitle.text = title + `${' '}`;
+    }
   };
   useEffect(updateTitle, [title]);
 };
