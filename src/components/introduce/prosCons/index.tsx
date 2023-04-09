f5dbf027f86f214b57826ff8642da3d6c0081183
alpha0212 <qkrcjfgml321@naver.com>
2023-04-10 05:06:21 +0900

    feat(component): props and cons component

--- src/components/introduce/prosCons/index.tsx
+++ src/components/introduce/prosCons/index.tsx
@@ -1,6 +1,18 @@
 import React from 'react';
 import * as S from '../prosCons/styled';
 
-export const propsConsComponent: React.FC = () => {
-  return <></>;
+interface PropsAndConsData {
+  text: string;
+  textDescription: string;
+}
+
+export const PropsConsComponent: React.FC<PropsAndConsData> = ({ text, textDescription }) => {
+  return (
+    <S.PropsConsElement>
+      <S.ElementTitle>{text}</S.ElementTitle>
+      <S.ElementDescContainer>
+        <S.ElementDesc>{textDescription}</S.ElementDesc>
+      </S.ElementDescContainer>
+    </S.PropsConsElement>
+  );
 };
